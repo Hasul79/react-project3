f52d8e8d0ce469f457c259766e7caa1ecaa6bf91
Hasul79 <hatuk2@mail.ru>
2022-08-19 03:29:31 +0400

    regist-login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router,  Route, Routes  } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Home from './components/home.component';
+import Nav from './components/nav.component';
+import Login from './components/login.component';
+import Register from './components/register.component';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Nav/>
+             <div className="auth-wrapper">
+                <div className="auth-inner">
+                  <Routes>
+                     <Route exact path="/" component={Home} />
+                     <Route exact path="/login" component={Login} />
+                     <Route exact path="/register" component={Register} />
+                  </Routes>
+         <Home/>
+            </div>
+         </div>
     </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/login.component.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+
+export default class Login extends Component {
+    render () {
+       return (
+		<h2> Դուք մուտք չեք գործել </h2>
+	   )
+ }
+}
