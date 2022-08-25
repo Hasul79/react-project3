35c2f565e2228cb9707ccba2e2ca2e335c9fcb97
Hasul79 <hatuk2@mail.ru>
2022-08-25 12:34:09 +0400

    form

--- src/Hasmik/Hasmiktable.js
+++ src/Hasmik/Hasmiktable.js
@@ -1,4 +1,68 @@
-function Hasmiktable(){
+import React, { useState} from 'react';
+import './Hasmiktable.css';
   
+
+const initialValues = {
+	userName: '',
+	userSurname: '',
+	userSalary: ''
 }
-export default Hasmiktable
+
+function Hasmiktable(){
+	const [userData, setUserData] = useState(initialValues);
+	
+
+	
+	console.log('userData', userData)
+	return (
+  <div clssName="wrapper">
+	<div className="wrapper-content">
+	<div className="form-data">
+                <form>
+					<input placeholder=" Write your name" 
+					onChange={(e) => setUserData((prevState) =>({
+						...prevState,
+						userName: e.target.value
+					}))} />
+					<input  placeholder="Write surname" 
+					onChange={(e) => setUserData((prevState) =>({
+						...prevState,
+						userSurname: e.target.value
+					}))} />
+					<input  placeholder="Write salary"
+					 onChange={(e) => setUserData((prevState) =>({
+						...prevState,
+						userSalart: e.target.value
+					}))}/>
+				
+					<div classNmae="buttons-wrapper">
+						<button type="reset">Clean</button>
+						<button type="submit">Add</button>
+					</div>
+			  
+			    </form>
+			  
+
+		    </div>
+
+		<div className="table-data">
+			<table>
+				<th> # </th>
+				<th> User Name </th>
+				<th> User Surname </th>
+				<th> User Salary </th>
+				<th>Actions</th>
+
+				<tbody>
+
+				</tbody>
+
+			</table>
+		</div>
+		    
+	</div>
+
+  </div>
+  )
+}
+export default Hasmiktable;
