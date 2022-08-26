1aa45f50a6c4864262ff67927c33e92c3d0e7972
Hasul79 <hatuk2@mail.ru>
2022-08-27 02:53:15 +0400

    remove

--- src/Hasmik/Hasmiktable.js
+++ src/Hasmik/Hasmiktable.js
@@ -1,6 +1,6 @@
 import React, { useState} from 'react';
 import './Hasmiktable.css';
-  
+import profile from './image/njdeh2.jpg';
 
 const initialValues = {
 	userName: '',
@@ -10,34 +10,60 @@ const initialValues = {
 
 function Hasmiktable(){
 	const [userData, setUserData] = useState(initialValues);
+	const [users, setUsers] = useState([]);
+
+   const isFilledFields = userData.userName && userData.userSurname && userData.userSalary
+
+	const handleSubmitUser = (e) => {
+       e.preventDefault();
+
+	if(isFilledFields) {
+	    setUsers((prevState) => [...prevState, userData]);
+	
+		setUserData(initialValues)
+	}  
+	
+	
+}
 	
 
-	
-	console.log('userData', userData)
+	console.log('userData ', userData);
+
 	return (
-  <div clssName="wrapper">
+  <div className="wrapper">
 	<div className="wrapper-content">
-	<div className="form-data">
-                <form>
+	   <div className="form-data">
+	     <div className="container-image">
+             <img src={profile} alt="profile" className="profile"/>
+			 </div>
+
+
+                <form onSubmit={handleSubmitUser}>
 					<input placeholder=" Write your name" 
 					onChange={(e) => setUserData((prevState) =>({
 						...prevState,
 						userName: e.target.value
-					}))} />
+					}))}
+					value={userData.userName} 
+					/>
 					<input  placeholder="Write surname" 
 					onChange={(e) => setUserData((prevState) =>({
 						...prevState,
 						userSurname: e.target.value
-					}))} />
+					}))} 
+					value={userData.userSurname} 
+					/>
 					<input  placeholder="Write salary"
 					 onChange={(e) => setUserData((prevState) =>({
 						...prevState,
-						userSalart: e.target.value
-					}))}/>
+						userSalary: e.target.value
+					}))}
+					value={userData.userSalary} 
+					/>
 				
 					<div classNmae="buttons-wrapper">
 						<button type="reset">Clean</button>
-						<button type="submit">Add</button>
+						<button disabled={!isFilledFields}  type="submit">Add</button>
 					</div>
 			  
 			    </form>
@@ -47,14 +73,31 @@ function Hasmiktable(){
 
 		<div className="table-data">
 			<table>
-				<th> # </th>
+				{/* <th> <span>&#8592;</span>HOME</th> */}
+				<th>#</th>
 				<th> User Name </th>
 				<th> User Surname </th>
 				<th> User Salary </th>
 				<th>Actions</th>
 
 				<tbody>
+                  {users.map((user, index) => (
+					<tr>
+					<td>{index + 1}</td>
+					<td>{user.userName}</td>
+					<td>{user.userSurname}</td>
+					<td>{user.userSalary}</td>
+					<td>
+						<div>
+							<button className="edit-action">edit</button>
+							<button  className="remove-action">remove</button>
+						</div>
+					</td>
+				</tr>
+				  ))}
+					
 
+					 
 				</tbody>
 
 			</table>
