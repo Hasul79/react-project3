9933818380388b8d151b6f5bcbb888d79f71506a
Hasul79 <hatuk2@mail.ru>
2022-10-03 22:35:22 +0400

    ADD_fetch

--- src/Hasmik/Hasmiktable.js
+++ src/Hasmik/Hasmiktable.js
@@ -1,21 +1,24 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Hasmiktable.css';
 import njdeh from './image/njdeh.jpg';
 import kapan from './image/kapan.jpg';
 import  prof from './image/a.jpg';
 import { Link } from "react-router-dom";
 
+
 const initialValues = {
-	userName: '',
-	userSurname: '',
-	userAge: '',
-	userSalary: ''
+	firstName: '',
+	surName: '',
+	age: '',
+	salary: ''
 }
 
 function Hasmiktable(){
+
+
+
 	// dark & light
 	const [isClicked, setIsClicked] = useState(false); //click-ով բացի նկարներն
-
 	const [userData, setUserData] = useState(initialValues);
 	const [users, setUsers] = useState([]);
 	const [editableUserData, setEditableUserData] = useState({
@@ -23,17 +26,28 @@ function Hasmiktable(){
 		userIndex: null
 	});
 
+	useEffect(() => {
+		fetch(`http://localhost:7777/users`)
+		.then(response => response.json())
+		.then(data => setUsers (data))
+	},[])
+	console.log(users)
+
+
 	// dark & light
 	const handleToggle = () => {
 		setIsClicked(!isClicked);
 	  };
 //  table function
          // Remove button ----------
-   const handleRemoveClick = (index) =>{
-       setUsers(users.filter((user, userIndex) => userIndex !== index));
+   const handleRemoveClick = (user) =>{
+       setUsers(users.filter((user, userIndex) => userIndex !== user.id));
+	   fetch(`http://localhost:7777/users/${user.id}` , {
+		method : 'DELETE'
+	   })
    }
 	 //input - պ/է լրացված լինի բոլոր input որ ակտիվ լինի
-	const isFilledFields = userData.userName && userData.userSurname && userData.userAge &&  userData.userSalary
+	const isFilledFields = userData.firstName && userData.surName && userData.age &&  userData.salary
         
 	     //Add button ------
 	const handleSubmitUser = (e) => {
@@ -49,8 +63,17 @@ function Hasmiktable(){
 			setEditableUserData({
 				isEdit: false,       //  user data տվյալների զրոյացում
 				userIndex: null      //index զրոյացում
+
 			})
+     fetch(`http://localhost:7777/users/${editedData[editableUserData.userIndex].user.id}`, {
+		method: 'PATCH',
+		  body: JSON.stringify(userData)
+	 })
 		} else {
+			fetch(`http://localhost:7777/users`, {
+				method: 'POST',
+			 	  body: JSON.stringify(userData)
+			})
 	        setUsers((prevState) => [...prevState, userData]);
 		}
 		// նախկին տվյալ + նոր user data
@@ -98,36 +121,36 @@ function Hasmiktable(){
 					<input className="input_name" placeholder=" Write your name" 
 					onChange={(e) => setUserData((prevState) =>({
 						...prevState,
-						userName: e.target.value
+						firstName: e.target.value
 					}))}
-					value={userData.userName} 
+					value={userData.firstName} 
 					/>
 
 					<input className="input_name" placeholder="Write surname" 
 					onChange={(e) => setUserData((prevState) =>({
 						...prevState,
-						userSurname: e.target.value
+						surName: e.target.value
 					}))} 
-					value={userData.userSurname} 
+					value={userData.surName} 
 					/>
 
 					<input className="input_name" placeholder="Write your age" 
 					onChange={(e) => setUserData((prevState) =>({
 						...prevState,
-						userAge: e.target.value
+						age: e.target.value
 					}))} 
-					value={userData.userAge} 
+					value={userData.age} 
 					/>
 
 					<input className="input_name"  placeholder="Write salary"
 					 onChange={(e) => setUserData((prevState) =>({
 						...prevState,
-						userSalary: e.target.value
+						salary: e.target.value
 					}))}
-					value={userData.userSalary} 
+					value={userData.salary} 
 					/>
 				
-					<div classNmae="buttons-wrapper">
+					<div className="buttons-wrapper">
 					
 						<button className="button_input" type="reset" >Clean</button>
 						<button className="button_input" disabled={!isFilledFields}  type="submit" >{editableUserData.isEdit ? `Edit` : `Add` }</button>
@@ -147,17 +170,17 @@ function Hasmiktable(){
 				<th className="thb">Actions</th>
 
 				<tbody>
-                  {users.map((user, index) => (
-				   <tr>
-						<td className="tdt">{index + 1}</td>
-						<td className="td">{user.userName}</td>
-						<td className="td">{user.userSurname}</td>
-						<td className="tda">{user.userAge}</td>
-						<td className="td">{user.userSalary}</td>
+                  {users.map((user) => (
+				   <tr key={user.id}>
+						<td className="tdt">{user.id }</td>
+						<td className="td">{user.firstName}</td>
+						<td className="td">{user.surName}</td>
+						<td className="tda">{user.age}</td>
+						<td className="td">{user.salary}</td>
 						<td className="td">
 							<div className='td-btn' >
-								<button className="edit-action button" onClick ={() => handleEditClick(user, index)}>Edit</button>
-								<button  className="remove-action button" onClick={() => handleRemoveClick(index)}>Delete</button>
+								<button className="edit-action button" onClick ={() => handleEditClick(user, user.id)}>Edit</button>
+								<button  className="remove-action button" onClick={() => handleRemoveClick(user.id)}>Delete</button>
 							</div>
 						</td>
 				    </tr>
